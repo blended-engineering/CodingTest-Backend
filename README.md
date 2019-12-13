49deed798443184c7700b0a5eb64c0d0efe18d60
Hae Jun Kim <dan.kimhaejun@gmail.com>
2019-12-13 15:05:02 +0900

    Setup Eslint, Connect S3, Connect MySQL DB instance

--- README.md
+++ README.md
@@ -4,8 +4,7 @@
 
 ![Test Image](./IMAGE.png)
 
-위 상품 상세 페이지를 위한  API 를 설계 및 작성하시면 됩니다.
-
+위 상품 상세 페이지를 위한 API 를 설계 및 작성하시면 됩니다.
 
 ### 유의 사항
 
@@ -15,7 +14,6 @@
 - Table Schema에 유의하시면서 작성하시면 됩니다.
 - "제품 정보", "가격 비교", "관련 상품", "색상", "리뷰"는 별도의 API로 분리하셔도 좋습니다.
 - 색상정보는 RGB (ex. #8fac9c) 로 저장됩니다.
-
 
 ## 우리는 이러한 점을 중심으로 평가합니다.
 
@@ -28,18 +26,17 @@
 아래 사항이 필수가 아닙니다. 필요하다면 추가로 다양한 Librery 나 도구를 사용하셔도 됩니다.
 
 - 필수 Tech Stack
-    - Javascript ( At least JS ES 6 )
-        - Node.js
-    - Git
-    - GraphQL or RESTFUL 
-    - DynamoDB or RDB
-    
+  - Javascript ( At least JS ES 6 )
+    - Node.js
+  - Git
+  - GraphQL or RESTFUL
+  - DynamoDB or RDB
 - 권장 Tech Stack
-    - Typescript
-    - Serverless
-
+  - Typescript
+  - Serverless
 
 ## 제출 방법
+
 - Github(Gitlab, bitbucket) 에 공개 저장소로 제출하시면 됩니다.
 - 실행 방법 및 프로젝트 소개를 README.md 파일에 정리해 주세요
 - 과제를 완료하시면 people@cosmochain.io로 연락 주시기 바랍니다.
---
+++ server/app.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+//동적 요청에 대한 응답을 보낼 때 etag 생성을 하지 않도록 설정
+app.set('etag', false);
+// 정적 요청에 대한 응답을 보낼 때 etag 생성을 하지 않도록 설정
+const options = { etag: false };
+app.use(express.static('public', options));
+
+app.use(cors());
+app.use(cookieParser());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+app.use(express.static('files'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
